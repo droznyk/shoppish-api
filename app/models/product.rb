9400fee15b142593822aaf604c58b9c73e1e871a
Patryk Stepien <droznyk@gmail.com>
2018-12-06 10:49:36 +0100

    Add product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates_presence_of :name, :description, :price, :quantity
+  validates_numericality_of :price, :quantity, greater_than_or_equal_to: 0.0
+  validates :description, length: { maximum: 800 }
 end
