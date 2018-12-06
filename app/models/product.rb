66f5e7534f4e1da504e78fb2ce870a86a77e6ab6
Patryk Stepien <droznyk@gmail.com>
2018-12-06 13:19:53 +0100

    Add integer validation for quantity

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   validates_presence_of :name, :description, :price, :quantity
   validates_numericality_of :price, :quantity, greater_than_or_equal_to: 0.0
   validates :description, length: { maximum: 800 }
+  validates :quantity, numericality: { only_integer: true }
 end
