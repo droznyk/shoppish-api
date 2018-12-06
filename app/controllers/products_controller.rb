99f30c09d0f89f8d21bdcb616ba75a85c8a8aafb
Patryk Stepien <droznyk@gmail.com>
2018-12-06 11:55:22 +0100

    Add show update destroy actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,38 @@
 class ProductsController < ApplicationController
+  before_action :fetch_product, only: %i[show update destroy]
   def index
     @products = Product.all
-    render json: @products
+    json_response(@products)
   end
 
   def create
     @product = Product.create!(product_params)
-    render json: @product
+    json_response(@product, :created)
+  end
+
+  def show
+    json_response(@product)
+  end
+
+  def update
+    json_response(:updated) if @product.update!(product_params)
+  end
+
+  def destroy
+    json_response(:deleted) if @product.destroy
   end
 
   private
 
   def product_params
     params.require(:product).permit(:name, :description, :price, :quantity)
+  end
+
+  def json_response(object, status= :ok)
+    render json: object, status: status
+  end
+
+  def fetch_product
+    @product = Product.find(params[:id])
   end
 end
