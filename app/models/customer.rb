bc69dd32e6f34c1590c1b18dd0bd0b70d047bb66
Patryk Stepien <droznyk@gmail.com>
2018-12-07 11:29:40 +0100

    Add validations for customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,8 @@
 class Customer < ApplicationRecord
   has_many :orders
+
+  validates_presence_of :name, :email, :street, :zip_code, :city
+  validates_uniqueness_of :email
+  validates :city, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
 end
