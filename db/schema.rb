c33e6b4d27fc0390e68c0ab32fdf910de5745e2a
Patryk Stepien <droznyk@gmail.com>
2018-12-07 10:51:17 +0100

    Create orders with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_092513) do
+ActiveRecord::Schema.define(version: 2018_12_07_094411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.date "order_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
---
+++ test/factories/orders.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :order do
+    order_date { "2018-12-07" }
+  end
+end
