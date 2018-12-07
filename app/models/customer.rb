c5e47420571b822465d3958c602148fe39e6bf65
Patryk Stepien <droznyk@gmail.com>
2018-12-07 12:15:46 +0100

    Associate customer with credit cards

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ApplicationRecord
   has_many :orders
+  has_many :credit_cards
 
   validates_presence_of :name, :email, :street, :zip_code, :city
   validates_uniqueness_of :email
