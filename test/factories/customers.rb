6d3947ccdbaaff9a7f5e67fd55339c0d7aa1173b
Patryk Stepien <droznyk@gmail.com>
2018-12-07 10:59:02 +0100

    Generate Customer model with migration

---
+++ test/factories/customers.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :customer do
+    name { "MyString" }
+    email { "MyString" }
+    street { "MyString" }
+    zip_code { "MyString" }
+    city { "MyString" }
+  end
+end
