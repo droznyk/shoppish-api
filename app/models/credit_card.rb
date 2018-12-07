3ff4a41b102ddbf1e898d5020adf71122259b6ce
Patryk Stepien <droznyk@gmail.com>
2018-12-07 14:19:23 +0100

    Add validations for CreditCard

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,3 +1,16 @@
 class CreditCard < ApplicationRecord
   belongs_to :customer
+
+  validates :number, presence: true, length: { is: 8 }, numericality: { only_integer: true }
+  validates :cvc, presence: true, length: { is: 3 }, numericality: { only_integer: true }
+  validates :expiration_date, presence: true
+  validate :expiration_date_cannot_be_in_the_past
+
+  private
+
+  def expiration_date_cannot_be_in_the_past
+    if expiration_date.present? && expiration_date < Date.today
+      errors.add(:expiration_date, "can't be in the past")
+    end
+  end
 end
