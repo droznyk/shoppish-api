9e1dab5538eef5236c4a72e5cb7f65c3977d6dbf
Patryk Stepien <droznyk@gmail.com>
2018-12-07 15:31:44 +0100

    Add routes for customers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :products, only: %i[index create show update destroy]
+  resources :customers, only: %i[create show update]
 end
