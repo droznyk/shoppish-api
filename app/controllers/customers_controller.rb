7bc99da8a89447dc1507646d20f9487e9ad0ebcc
Patryk Stepien <droznyk@gmail.com>
2018-12-07 16:47:39 +0100

    Create customers controller

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,37 @@
+class CustomersController < ApplicationController
+  before_action :fetch_customer, only: %i[show update]
+  def create
+    if customer_params.include?(:credit_card)
+      @customer = CreateCustomerWithCreditCardService.new(customer_params).call
+    else
+      @customer = CreateCustomerService.new(customer_params).call
+    end
+    @customer.persisted? ? json_response(@customer, :created) : respond_with_error(@customer)
+  end
+
+  def show
+    json_response(@customer)
+  end
+
+  def update
+    if @customer.update(customer_params)
+      json_response(@customer)
+    else
+      respond_with_error(@customer)
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :street, :zip_code, :city)
+  end
+
+  def fetch_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  def json_response(object, status= :ok)
+    render json: object, status: status
+  end
+end
