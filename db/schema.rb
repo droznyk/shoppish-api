602f04228bf922e5d6eb72923a0695d23a42cc15
Patryk Stepien <droznyk@gmail.com>
2018-12-07 11:03:49 +0100

    Add customer_id to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_07_095758) do
+ActiveRecord::Schema.define(version: 2018_12_07_100152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2018_12_07_095758) do
     t.date "order_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "customer_id"
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -40,4 +42,5 @@ ActiveRecord::Schema.define(version: 2018_12_07_095758) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "orders", "customers"
 end
