063935f857ffb3e1a6a51bd092a74e20216ae126
Patryk Stepien <droznyk@gmail.com>
2018-12-10 12:45:35 +0100

    Add service for creating credit cards

---
+++ app/services/create_credit_card_service.rb
@@ -0,0 +1,20 @@
+class CreateCreditCardService
+  def initialize(credit_card_params)
+    @customer = Customer.find(credit_card_params[:customer_id])
+    @credit_card_params = credit_card_params
+  end
+
+  def call
+    @credit_card_params[:expiration_date] = set_expiration_date(@credit_card_params[:expiration_date])
+    @credit_card = @customer.credit_cards.create(@credit_card_params)
+    return @credit_card
+  end
+
+  private
+
+  def set_expiration_date(expiration_date)
+    month, year = expiration_date.split('/').map(&:to_i)
+    year = "20#{year}".to_i
+    expiration_date = DateTime.new(year, month, -1)
+  end
+end
