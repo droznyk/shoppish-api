eb5e0ec11676b0b4f4ec11586852b601e0ef3685
Patryk Stepien <droznyk@gmail.com>
2018-12-10 12:41:46 +0100

    Add nested routes for user credit cards

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :products, only: %i[index create show update destroy]
-  resources :customers, only: %i[create show update]
+  resources :customers, only: %i[create show update] do
+    resources :credit_cards, only: %i[index create show update destroy]
+  end
 end
