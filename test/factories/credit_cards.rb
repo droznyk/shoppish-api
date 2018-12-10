337a3db4d987e78157f2fb108a9f82774a54603d
Patryk Stepien <droznyk@gmail.com>
2018-12-10 15:34:49 +0100

    Refactor expiration_date value for credit card factory

--- test/factories/credit_cards.rb
+++ test/factories/credit_cards.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :credit_card do
     number { 12345678 }
     cvc { 123 }
-    expiration_date { "2018-12-07" }
-    customer { 1 }
+    expiration_date { "#{Time.now}" }
+    customer
   end
 end
