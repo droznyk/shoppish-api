e2e8a2f14e32154fcdabdbcf2f2437dec5515024
Patryk Stepien <droznyk@gmail.com>
2018-12-11 11:01:44 +0100

    Associate order and product with order_position

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  has_many :order_positions
+  has_many :products, through: :order_positions
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :order_positions
+  has_many :orders, through: :order_positions
+  
   validates_presence_of :name, :description, :price, :quantity
   validates_numericality_of :price, :quantity, greater_than_or_equal_to: 0.0
   validates :description, length: { maximum: 800 }
