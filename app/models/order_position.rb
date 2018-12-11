f8e955e42e6823cd351597a9c0fc126b7380d297
Patryk Stepien <droznyk@gmail.com>
2018-12-11 11:12:13 +0100

    Add validations for order position

--- app/models/order_position.rb
+++ app/models/order_position.rb
@@ -1,4 +1,7 @@
 class OrderPosition < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validates :product_quantity, presence: true. numericality: { only_integer: true }
+  validates :price, presence: true, numericality: { greater_than: 0.0 }
 end
