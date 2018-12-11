95303b9081fae69390277ce3937fdeaf50e7f20a
Patryk Stepien <droznyk@gmail.com>
2018-12-11 13:13:10 +0100

    Fix typo in order position validation

--- app/models/order_position.rb
+++ app/models/order_position.rb
@@ -2,6 +2,6 @@ class OrderPosition < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
-  validates :product_quantity, presence: true. numericality: { only_integer: true }
+  validates :product_quantity, presence: true, numericality: { only_integer: true }
   validates :price, presence: true, numericality: { greater_than: 0.0 }
 end
