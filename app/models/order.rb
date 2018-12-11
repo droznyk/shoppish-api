c2ba632578a161b4a24538e2e3fb80f5d919b432
Patryk Stepien <droznyk@gmail.com>
2018-12-11 14:40:28 +0100

    Add nested attributes for order positions in order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_positions
   has_many :products, through: :order_positions
+  accepts_nested_attributes_for :order_positions
 end
