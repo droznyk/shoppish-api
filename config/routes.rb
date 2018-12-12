a043fab3667dad10937fd5ba315634227f033ee0
Patryk Stepien <droznyk@gmail.com>
2018-12-12 15:06:16 +0100

    Move orders routing outside customer and create form routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :products, only: %i[index create show update destroy]
   resources :customers, only: %i[create show update] do
     resources :credit_cards, only: %i[index create show destroy]
-    resources :orders, only: %i[index create show destroy]
   end
+
+  resources :orders, only: %i[index show destroy]
+  post '/order/step1', to: 'orders#form_step1'
+  post '/order/step2', to: 'orders#form_step2'
+  post '/order/step3', to: 'orders#form_step3'
 end
