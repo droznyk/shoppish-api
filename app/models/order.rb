c2446966147a6f5adc3cab3ad30afdec1434eae6
Patryk Stepien <droznyk@gmail.com>
2018-12-12 15:00:12 +0100

    Accept null on customer_id FK at orders table

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  belongs_to :customer
+  belongs_to :customer, optional: true
   has_many :order_positions
   has_many :products, through: :order_positions
   accepts_nested_attributes_for :order_positions
